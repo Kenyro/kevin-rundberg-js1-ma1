2a9f716bcb0392035bf785075590bfc71aa94809
Kenyro <73535536+Kenyro@users.noreply.github.com>
2022-01-24 23:37:21 +0100

    q1

--- js/script.js
+++ js/script.js
@@ -1,14 +1,39 @@
 const cats = [
-    {
-        name: "Blob",
-        age: 10
-    },
-    {
-        name: "Harold",
-    },
-    {
-        name: "Blurt",
-        age: 21
-    }
+  {
+    name: "Blob",
+    age: 10,
+  },
+  {
+    name: "Harold",
+  },
+  {
+    name: "Blurt",
+    age: 21,
+  },
 ];
 
+// Question 1
+
+const cat = {
+  meow: function () {
+    console.log("Meow!");
+  },
+};
+
+cat.meow();
+
+// Question 2
+
+const heading = document.querySelector("h3");
+
+// Question 3
+
+// Question 4
+
+// Question 5
+
+// Question 6
+
+// Question 7
+
+// Question 8
